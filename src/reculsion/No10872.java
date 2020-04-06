c0c8f7ba4c62145f190919275d334f8f0d96c252
hyunho058 <hyunho058@naver.com>
2020-04-06 17:44:20 +0900

    Add No10872

--- src/reculsion/No10872.java
+++ src/reculsion/No10872.java
@@ -6,26 +6,16 @@ public class No10872 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int number = scan.nextInt();
-		int result = 1;
-		if (number >= 0 && number <= 12) {
-			for (int i = 1; i <= number; i++) {
-				result *= i;
-			}
-			System.out.println(result);
-		}
-		System.out.println(function(10));
-		
+		System.out.println(function(number));
 	}
+
 	static int function(int number) {
-		int data = number;
-		int count = 0;
-		int result = 1;
-		if(number == 0) {
-			return 0;
-		}else {
-			System.out.println("??");
-			return function(number-1);
+		if (number == 0) {
+			return 1;
+		} else {
+			return number * function(number - 1);
 		}
 	}
 }
