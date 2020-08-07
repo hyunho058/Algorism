92af9f070e0fb642cc292b91dff5139953c95516
hyunho058 <hyunho058@naver.com>
2020-08-07 19:27:54 +0900

    Test Commit

--- src/string/No11720.java
+++ src/string/No11720.java
@@ -2,6 +2,11 @@ package string;
 
 import java.util.Scanner;
 
+/**
+ * N개의 숫자가 공백 없이 쓰여있다. 
+ * 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+ */
+
 public class No11720 {
 
 	public static void main(String[] args) {
