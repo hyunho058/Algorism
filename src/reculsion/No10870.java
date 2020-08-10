e711c0b51ad131a0261c970d866307672c69c30b
hyunho058 <hyunho058@naver.com>
2020-08-10 17:18:34 +0900

    Add Recursion call

--- src/reculsion/No10870.java
+++ src/reculsion/No10870.java
@@ -1,13 +1,26 @@
 package reculsion;
 
+import java.util.Scanner;
+
 public class No10870 {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		System.out.println(function(1, 1, number));
 	}
-	
-	public int funtion(int fibonacciNumber) {
-		
-		return 0;
+
+	private static int function(int num1, int num2, int number) {
+		if(number == 0) {
+			return 0;
+		}
+		else if(number == 1) {
+			return num1; 
+		}
+		else {
+			int sum= num1+ num2;
+			number-= 1;
+			return function(num2, sum, number);
+		}
 	}
 }
--- src/reculsion/No10872.java
+++ src/reculsion/No10872.java
@@ -6,16 +6,14 @@ public class No10872 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
 		int number = scan.nextInt();
 		System.out.println(function(number));
 	}
-
-	static int function(int number) {
-		if (number == 0) {
+	public static int function(int number) {
+		if(number == 0) {	
 			return 1;
-		} else {
-			return number * function(number - 1);
+		}else{
+			return number * function(number-1);
 		}
 	}
 }
