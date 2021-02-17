eec44aaabd76fcc88fde986a3da08a6fdeaebb57
hyunho058 <hyunho058@naver.com>
2021-02-17 21:53:50 +0900

    [Algorithm] Update until one

---
+++ kotlin/src/main/java/greedy/UntilOne.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.util.Scanner;
+
+public class UntilOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        System.out.println("RESULT=="+getResult(N, K));
+    }
+
+    public static int getResult(int N, int K){
+        int count = 0;
+        while (true){
+            if (N%K != 0){
+                N -= 1;
+            }else {
+                N /= K;
+            }
+            count++;
+            if (N == 1){
+                break;
+            }
+        }
+        return count;
+    }
+}
